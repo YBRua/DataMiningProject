08724a914205787049a6e05d777724dc2c3f6151
YBRua <ybiur0929@163.com>
2022-01-08 17:49:31 +0800

    tuning

--- end2end.py
+++ end2end.py
@@ -1,3 +1,4 @@
+import sys
 import itertools
 from typing import List
 import torch
@@ -21,8 +22,8 @@ def knn(x, k):
     inner = -2*torch.matmul(x.transpose(2, 1), x)
     xx = torch.sum(x**2, dim=1, keepdim=True)
     pairwise_distance = -xx - inner - xx.transpose(2, 1)
- 
-    idx = pairwise_distance.topk(k=k, dim=-1)[1]   # (batch_size, num_points, k)
+
+    idx = pairwise_distance.topk(k=k, dim=-1)[1]  # (batch_size, num_points, k)
     return idx
 
 
@@ -31,24 +32,25 @@ def get_graph_feature(x, k=20, idx=None):
     num_points = x.size(2)
     x = x.view(batch_size, -1, num_points)
     if idx is None:
-        idx = knn(x, k=k)   # (batch_size, num_points, k)
+        idx = knn(x, k=k)  # (batch_size, num_points, k)
     device = x.device
 
-    idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1)*num_points
+    idx_base = torch.arange(0, batch_size, device=device)\
+        .view(-1, 1, 1) * num_points
 
     idx = idx + idx_base
 
     idx = idx.view(-1)
- 
+
     _, num_dims, _ = x.size()
 
-    x = x.transpose(2, 1).contiguous()   # (batch_size, num_points, num_dims)  -> (batch_size*num_points, num_dims) #   batch_size * num_points * k + range(0, batch_size*num_points)
+    x = x.transpose(2, 1).contiguous()  # (batch_size, num_points, num_dims)  -> (batch_size*num_points, num_dims) #   batch_size * num_points * k + range(0, batch_size*num_points)
     feature = x.view(batch_size*num_points, -1)[idx, :]
     feature = feature.view(batch_size, num_points, k, num_dims) 
     x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
-    
+
     feature = torch.cat((feature-x, x), dim=3).permute(0, 3, 1, 2).contiguous()
-  
+
     return feature
 
 
@@ -59,7 +61,7 @@ class DGCNN(nn.Module):
         self.k = args.k
         self.input_dims = args.input_dims
         output_channels = args.output_channels
-        
+
         self.bn1 = nn.BatchNorm2d(64)
         self.bn2 = nn.BatchNorm2d(64)
         self.bn3 = nn.BatchNorm2d(128)
@@ -75,16 +77,13 @@ class DGCNN(nn.Module):
         self.conv3 = nn.Sequential(nn.Conv2d(64*2, 128, kernel_size=1, bias=False),
                                    self.bn3,
                                    nn.LeakyReLU(negative_slope=0.2))
-        self.conv4 = nn.Sequential(nn.Conv2d(128*2, 256, kernel_size=1, bias=False),
-                                   self.bn4,
-                                   nn.LeakyReLU(negative_slope=0.2))
-        self.conv5 = nn.Sequential(nn.Conv1d(512, args.emb_dims, kernel_size=1, bias=False),
+        # self.conv4 = nn.Sequential(nn.Conv2d(128*2, 256, kernel_size=1, bias=False),
+        #                            self.bn4,
+        #                            nn.LeakyReLU(negative_slope=0.2))
+        self.conv5 = nn.Sequential(nn.Conv1d(128, args.emb_dims, kernel_size=1, bias=False),
                                    self.bn5,
                                    nn.LeakyReLU(negative_slope=0.2))
-        self.linear1 = nn.Conv1d(args.emb_dims, 512, 1, bias=False)
-        self.bn6 = nn.BatchNorm1d(512)
-        self.dp1 = nn.Dropout(p=args.dropout)
-        self.linear2 = nn.Conv1d(512, 256, 1)
+        self.linear2 = nn.Conv1d(args.emb_dims, 256, 1)
         self.bn7 = nn.BatchNorm1d(256)
         self.dp2 = nn.Dropout(p=args.dropout)
         self.linear3 = nn.Conv1d(256, output_channels, 1)
@@ -98,20 +97,21 @@ class DGCNN(nn.Module):
         x = self.conv2(x)
         x2 = x.max(dim=-1, keepdim=False)[0]
 
-        x = get_graph_feature(x2, k=self.k)
-        x = self.conv3(x)
-        x3 = x.max(dim=-1, keepdim=False)[0]
+        # x = get_graph_feature(x2, k=self.k)
+        # x = self.conv3(x)
+        # x3 = x.max(dim=-1, keepdim=False)[0]
 
-        x = get_graph_feature(x3, k=self.k)
-        x = self.conv4(x)
-        x4 = x.max(dim=-1, keepdim=False)[0]
+        # x = get_graph_feature(x3, k=self.k)
+        # x = self.conv4(x)
+        # x4 = x.max(dim=-1, keepdim=False)[0]
 
-        x = torch.cat((x1, x2, x3, x4), dim=1)
+        # x = torch.cat((x1, x2, x3, x4), dim=1)
+        x = torch.cat((x1, x2), dim=1)
 
         x = self.conv5(x)
 
-        x = F.leaky_relu(self.bn6(self.linear1(x)), negative_slope=0.2)
-        x = self.dp1(x)
+        # x = F.leaky_relu(self.bn6(self.linear1(x)), negative_slope=0.2)
+        # x = self.dp1(x)
         x = F.leaky_relu(self.bn7(self.linear2(x)), negative_slope=0.2)
         x = self.dp2(x)
         x = self.linear3(x)
@@ -126,18 +126,21 @@ class Net(nn.Module):
         self.dgcnn = DGCNN(SimpleNamespace(
             k=9,
             emb_dims=128,
-            input_dims=33,
+            input_dims=64,
             output_channels=1,
             dropout=0.3
         ))
 
     def forward(self, user_ids, item_ids, labels=None):
         user = self.user_embed(user_ids)
-        user = torch.cat([user, torch.ones_like(user[..., :1])], -1)
+        # user = torch.cat([user, torch.ones_like(user[..., :1])], -1)
         item = self.item_embed(item_ids)
-        item = torch.cat([item, torch.zeros_like(item[..., :1])], -1)
-        hodgepodge = torch.cat([user.unsqueeze(1), item], 1).transpose(1, 2)
-        return self.dgcnn(hodgepodge).squeeze(1)[..., 1:]
+        # item = torch.cat([item, torch.zeros_like(item[..., :1])], -1)
+        hodgepodge = torch.cat(
+            [user.unsqueeze(1).expand_as(item), item],
+            -1
+        ).transpose(1, 2)
+        return self.dgcnn(hodgepodge).squeeze(1)
 
 
 class Rekommand(IterableDataset):
@@ -196,20 +199,42 @@ def main():
     test = Rekommand(test, full=True)
     model = Net().to(device)
     opt = torch.optim.Adam(model.parameters())
-    B = 32
+    B = int(sys.argv[-1])
     train_loader = DataLoader(train, B, num_workers=2)
     test_loader = DataLoader(test, B, num_workers=2)
     best_acc = -1
     for epoch in range(100):
-        train_sub = TruncatedIter(train_loader, train.__length_hint__() // B + 1)
-        train_stats = run_epoch(model, train_sub, [Loss(), NDCG3()], epoch, opt)
-        val_stats = run_epoch(model, test_loader, [Loss(), NDCG3()], epoch)
+        train_sub = TruncatedIter(
+            train_loader,
+            train.__length_hint__() // B + 1
+        )
+        train_stats = run_epoch(
+            model,
+            train_sub,
+            [Loss(), NDCG3()],
+            epoch,
+            opt
+        )
+        val_stats = run_epoch(
+            model,
+            test_loader,
+            [Loss(), NDCG3()],
+            epoch
+        )
         if epoch == 0:
-            write_log("epoch", "train_loss", "val_loss", "train_ndcg@3", "val_ndcg@3")
+            write_log(
+                "epoch",
+                "train_loss",
+                "val_loss",
+                "train_ndcg@3",
+                "val_ndcg@3"
+            )
         write_log(
             epoch,
-            train_stats['Loss'], val_stats['Loss'],
-            train_stats['NDCG3'], val_stats['NDCG3'],
+            train_stats['Loss'],
+            val_stats['Loss'],
+            train_stats['NDCG3'],
+            val_stats['NDCG3'],
         )
         if val_stats['NDCG3'] > best_acc:
             best_acc = val_stats['NDCG3']
