3bba12a25780cf1541097f92386f8c4227799408
eliphatfs <flandre@scarletx.cn>
2022-01-11 20:19:32 +0800

    Some attempts. 60.42.

--- reprod.py
+++ reprod.py
@@ -11,6 +11,7 @@ from types import SimpleNamespace
 from torch.utils.data import DataLoader, IterableDataset
 
 from baseline.metric import batch_ndcg_torch
+from dgcnn import DGCNN
 import data_io
 from common import *
 
@@ -38,8 +39,7 @@ class SelfAttention(nn.Module):
         return torch.einsum("...nmh,...mhd->...nhd", attn, v).flatten(-2)
 
 
-
-class Net(nn.Module):
+class SAF(nn.Module):
     def __init__(self):
         super().__init__()
         self.user_embed = nn.Embedding(90000, 300)
@@ -53,7 +53,7 @@ class Net(nn.Module):
         self.linear = nn.Linear(300, 1)
         self.norm = nn.LayerNorm(300)
 
-    def forward(self, user_ids, item_ids, user_features, item_features, labels=None):
+    def encode(self, user_features, item_features):
         # bNMd
         item = self.item_embed(item_features)
         # bL4d
@@ -61,7 +61,31 @@ class Net(nn.Module):
         user = user.unsqueeze(1).repeat(1, item.shape[1], 1, 1)  # bNLd
         sa_pre = torch.cat([user, item], 2)  # bN (L+M) d
         sa_post = self.norm(self.proj(self.attn(sa_pre)) + sa_pre).mean(-2)  # bNd
+        return sa_post
+
+    def forward(self, user_ids, item_ids, user_features, item_features, labels=None):
+        sa_post = self.encode(user_features, item_features)
         return self.linear(self.drop(sa_post)).squeeze()
+
+
+class RankingAwareNet(nn.Module):
+    def __init__(self, encoder: SAF):
+        super().__init__()
+        self.encoder = encoder
+        self.impf = nn.Sequential(
+            nn.Linear(300, 1024),
+            nn.LayerNorm(1024), nn.ReLU(),
+        )
+        self.cls = nn.Sequential(
+            nn.Dropout(), nn.Linear(1324, 1)
+        )
+
+    def forward(self, user_ids, item_ids, user_features, item_features, labels=None):
+        with torch.no_grad():
+            sa_post = self.encoder.encode(user_features, item_features)
+        impf = self.impf(sa_post).max(-2)[0].unsqueeze(-2).repeat(1, sa_post.shape[1], 1)
+        pf = torch.cat([impf, sa_post], dim=-1)
+        return self.cls(pf).squeeze()
 
 
 def get_train_instances(train, ufd, ifd):
@@ -99,7 +123,9 @@ class Rekommand(IterableDataset):
         self.ifd = item_feature_dict
         self.ufd = user_feature_dict
         self.valid_items = list(self.ifd.keys())
-        self.cum_weights = [*itertools.accumulate(map(lambda x: len(x.positives), self.entries))]
+        self.cum_weights = [*itertools.accumulate(map(
+            lambda x: len(x.positives) + len(x.negatives), self.entries
+        ))]
 
     def __iter__(self):
         rng = len(self.entries) if self.full else 2 ** 30
@@ -110,13 +136,9 @@ class Rekommand(IterableDataset):
             if not self.full:
                 if not len(entry.positives):
                     continue
-                if random.random() < 0.2:
-                    entry.positives = random.choices(entry.positives, k=1)
-                    entry.negatives = []
-                else:
-                    entry.positives = []
-                    entry.negatives = random.choices(entry.negatives, k=1)
-                # entry.negatives = random.choices(self.valid_items, k=self.npe)
+                entry.positives = random.choices(entry.positives, k=self.ppe)
+                # entry.negatives = random.choices(entry.negatives, k=self.npe)
+                entry.negatives = random.choices(self.valid_items, k=self.npe)
             items = numpy.array(list(entry.positives) + list(entry.negatives))
             item_features = numpy.array([self.ifd[x][:4] for x in items])
             user_features = numpy.array(self.ufd[entry.id]).reshape(-1, 4)
@@ -146,21 +168,27 @@ class NDCG3(Metric):
 
 def main():
     train_raw = pickle.load(open("data/train.pkl", "rb"))
+    train_smp = data_io.load_train_entries('data/bookcross.train.rating')
     test = data_io.load_test_entries('data/bookcross', False)
     features_dict = pickle.load(open('data/book_info_bookcross', 'rb'))
     user_features_dict = pickle.load(open('data/user_hist_withinfo_bookcross', 'rb'))
+    train_smp = Rekommand(train_smp, features_dict, user_features_dict)
     test = Rekommand(test, features_dict, user_features_dict, full=True)
     B = 64
+    train_loader_smp = DataLoader(train_smp, B, num_workers=4)
     test_loader = DataLoader(test, B, num_workers=1)
 
-    model = Net().to(device)
-    target = Net().to(device)
+    model = RankingAwareNet(SAF()).to(device)
+    target = RankingAwareNet(SAF()).to(device)
+    enc_opt = torch.optim.Adam(model.encoder.parameters())
     opt = torch.optim.Adam(model.parameters())
     best_acc = -1
     train = get_train_instances(train_raw, user_features_dict, features_dict)
     for epoch in range(20):
         train_loader = DataLoader(train, B, num_workers=2, shuffle=True)
-        train_stats = run_epoch(model, train_loader, [Loss()], epoch, opt)
+        ent_stats = run_epoch(model.encoder, train_loader, [Loss()], epoch, enc_opt)
+        train_sub = TruncatedIter(train_loader_smp, train_smp.__length_hint__() // B + 1)
+        frt_stats = run_epoch(model, train_sub, [Loss()], epoch, opt)
         lerp = epoch / (epoch + 1)
         with torch.no_grad():
             target.load_state_dict(merge_state_dicts([
@@ -169,11 +197,11 @@ def main():
             ]))
         val_stats = run_epoch(target, test_loader, [Loss(), NDCG3()], epoch)
         if epoch == 0:
-            write_log("epoch", "train_loss", "val_loss", "val_ndcg@3")
+            write_log("epoch", "encoder_loss", "finetune_loss", "val_loss", "val_ndcg@3")
         write_log(
             epoch,
-            train_stats['Loss'], val_stats['Loss'],
-            val_stats['NDCG3'],
+            ent_stats['Loss'], frt_stats['Loss'],
+            val_stats['Loss'], val_stats['NDCG3']
         )
         if val_stats['NDCG3'] > best_acc:
             best_acc = val_stats['NDCG3']
